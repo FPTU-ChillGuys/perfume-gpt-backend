3ef46fedbfcffdb7f3d1a982ca566790cfaef602
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2026-01-30 23:29:44 +0700

    Update usings in ProductsController for auth and DTOs
    Added Microsoft.AspNetCore.Authorization and consolidated PerfumeGPT DTO namespaces in ProductsController.cs to enable authorization features and access to media and product DTOs.
    
    Add auth and DTO namespaces to ProductsController
    
    Added Microsoft.AspNetCore.Authorization and PerfumeGPT DTO namespaces to ProductsController to enable authorization features and structured request/response handling for media and product operations.

--- PerfumeGPT.API/Controllers/ProductsController.cs
+++ PerfumeGPT.API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using PerfumeGPT.API.Controllers.Base;
 using PerfumeGPT.Application.DTOs.Requests.Media;
 using PerfumeGPT.Application.DTOs.Requests.Products;
