95c204220f363ac670cf94c53091b67d4b26886a
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2026-01-30 13:30:41 +0700

    Refactor product similarity search and embedding fields
    Refactor product similarity search logic to filter out deleted products earlier, add debug output for vector distances, and prepare for threshold-based filtering. Also, expand embedding generation to include top, middle, and base note fields for improved product representation.

--- PerfumeGPT.Persistence/Repositories/ProductRepository.cs
+++ PerfumeGPT.Persistence/Repositories/ProductRepository.cs
@@ -65,15 +65,37 @@ namespace PerfumeGPT.Persistence.Repositories
 
             var sqlVector = new SqlVector<float>(searchEmbedding);
 
-            var topSimilarProductsQuery = _context.Products.OrderBy(p => EF.Functions.VectorDistance("cosine", p.Embedding!.Value, sqlVector)).Include(p => p.Brand)
+            var productQuery = _context.Products
+                .Where(p => !p.IsDeleted);
+
+
+            var threshold = 0.5;
+            var metricType = "cosine";
+            var topSimilarProductsQuery = productQuery
+                .Include(p => p.Brand)
                 .Include(p => p.Category)
                 .Include(p => p.FragranceFamily)
-                .Where(p => !p.IsDeleted).AsNoTracking();
+                //.Where(p => EF.Functions.VectorDistance(metricType, p.Embedding!.Value, sqlVector) <= threshold)
+                .OrderBy(p => EF.Functions.VectorDistance(metricType, p.Embedding!.Value, sqlVector))
+                .AsNoTracking();
+
+            // Get distance from the most similar product to use for total count calculation
+            var productDistance = await productQuery
+                .Select(p => new
+                {
+                    ProductName = p.Name,
+                    Distance = EF.Functions.VectorDistance(metricType, p.Embedding!.Value, sqlVector)
+                })
+                .ToListAsync();
+
+            foreach (var item in productDistance)
+            {
+                Console.WriteLine($"{item.ProductName}: {item.Distance} compare to threshold: {threshold}");
+            }
 
             var totalCount = await _context.Products.CountAsync();
 
             var items = await topSimilarProductsQuery
-                .OrderByDescending(p => p.CreatedAt)
                 .Skip((request.PageNumber - 1) * request.PageSize)
                 .Take(request.PageSize)
                 .ToListAsync();
@@ -118,7 +140,7 @@ namespace PerfumeGPT.Persistence.Repositories
             if (product != null)
             {
                 // Combine all relevant text fields for embedding
-                var textToEmbed = $"Name:{product?.Name} Description:{product?.Description} FragranceFamily:{product?.FragranceFamily?.Name} Cateogory:{product?.Category?.Name} Brand:{product?.Brand?.Name}";
+                var textToEmbed = $"Name:{product?.Name} Description:{product?.Description} FragranceFamily:{product?.FragranceFamily?.Name} Cateogory:{product?.Category?.Name} Brand:{product?.Brand?.Name} TopNote:{product?.TopNotes} MiddleNote:{product?.MiddleNotes} BaseNote:{product?.BaseNotes}";
                 var embedding = await embeddingGenerator.GenerateVectorAsync(textToEmbed ?? string.Empty);
                 product!.Embedding = new SqlVector<float>(embedding);
                 _context.Update(product);
