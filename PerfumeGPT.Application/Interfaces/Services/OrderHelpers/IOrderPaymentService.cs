4fb1258883d847033be28a1f3351b9c75a38ad06
Phuocws <nhp309@gmail.com>
2026-04-09 23:44:39 +0700

    fix: missing interface push.

--- PerfumeGPT.Application/Interfaces/Services/OrderHelpers/IOrderPaymentService.cs
+++ PerfumeGPT.Application/Interfaces/Services/OrderHelpers/IOrderPaymentService.cs
@@ -1,3 +1,4 @@
+using PerfumeGPT.Application.DTOs.Responses.Payments;
 using PerfumeGPT.Domain.Entities;
 using PerfumeGPT.Domain.Enums;
 
@@ -5,6 +6,6 @@ namespace PerfumeGPT.Application.Interfaces.Services.OrderHelpers
 {
 	public interface IOrderPaymentService
 	{
-		Task<string> CreatePaymentAndGenerateResponseAsync(Order order, decimal amount, PaymentMethod paymentMethod, string? posSessionId);
+		Task<CreatePaymentResponseDto> CreatePaymentAndGenerateResponseAsync(Order order, decimal amount, PaymentMethod paymentMethod, string? posSessionId);
 	}
 }
