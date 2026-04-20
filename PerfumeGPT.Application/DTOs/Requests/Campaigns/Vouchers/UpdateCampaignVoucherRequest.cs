ed794db2f07596153bd6177fae99741f69f5c106
Phuocws <nhp309@gmail.com>
2026-04-20 15:55:01 +0700

    fix: add nullable keyword to update campaign voucher.

--- PerfumeGPT.Application/DTOs/Requests/Campaigns/Vouchers/UpdateCampaignVoucherRequest.cs
+++ PerfumeGPT.Application/DTOs/Requests/Campaigns/Vouchers/UpdateCampaignVoucherRequest.cs
@@ -7,7 +7,7 @@ namespace PerfumeGPT.Application.DTOs.Requests.Campaigns.Vouchers
 		public Guid? Id { get; init; }
 		public required string Code { get; init; }
 		public decimal DiscountValue { get; init; }
-		public PromotionType TargetItemType { get; init; }
+		public PromotionType? TargetItemType { get; init; }
 		public DiscountType DiscountType { get; init; }
 		public VoucherType ApplyType { get; init; }
 		public decimal? MaxDiscountAmount { get; init; }
